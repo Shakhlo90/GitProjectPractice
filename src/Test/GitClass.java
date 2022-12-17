7ee03d30ef9dacf73967555934d5d5a24ee03e78
Shakhlo <110711642+Shakhlo90@users.noreply.github.com>
2022-12-17 10:58:53 -0500

    version2

--- src/Test/GitClass.java
+++ src/Test/GitClass.java
@@ -3,5 +3,6 @@ package Test;
 public class GitClass {
     public static void main(String[] args) {
         System.out.println("First version is completed");
+        System.out.println("the version is complete");
     }
 }
